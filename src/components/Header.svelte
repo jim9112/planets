cc8790eb30fb7f9a86a6ece0bbe9d0822f1d6cbc
jim9112 <jim9112@gmail.com>
2021-06-24 09:10:48 -0400

    add all of the planets

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,1 +1,14 @@
+<script lang="ts">
+    export let findPlanet: any
+</script>
 <h1>The Planets</h1>
+<ul>
+    <li on:click={findPlanet("Mercury")}>Mercury</li>
+    <li on:click={findPlanet("Venus")}>Venus</li>
+    <li on:click={findPlanet("Earth")}>Earth</li>
+    <li on:click={findPlanet("Mars")}>Mars</li>
+    <li on:click={findPlanet("Jupiter")}>Jupiter</li>
+    <li on:click={findPlanet("Saturn")}>Saturn</li>
+    <li on:click={findPlanet("Uranus")}>Uranus</li>
+    <li on:click={findPlanet("Neptune")}>Neptune</li>
+</ul>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,24 @@
 <script lang="ts">
     import Header from '../components/Header.svelte'
     import data from '../lib/data.json'
-    let found = data.find(planet => planet.name === "Earth");
-    console.log(found)
+    let found
+    let loading = true
+    // find chosen planet in JSON file
+    const findPlanet = (planetName) => {
+        found = data.find(planet => planet.name === planetName)
+        loading = false
+        console.log(found)
+        
+    }
+    // To Do: make a default planet pop up
+    // found = findPlanet('Earth')
 </script>
 
-    <Header />
+    <Header findPlanet={findPlanet} />
+    {#if !loading}
+	<h1>{found?.name}</h1>
+{/if}
+    
  
 
 <style>
