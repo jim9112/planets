2edc59027f12fd78688848ce4443bb12048b5725
jim9112 <jim9112@gmail.com>
2021-06-24 09:32:15 -0400

    initial header layout

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,14 +1,35 @@
 <script lang="ts">
     export let findPlanet: any
 </script>
-<h1>The Planets</h1>
-<ul>
-    <li on:click={findPlanet("Mercury")}>Mercury</li>
-    <li on:click={findPlanet("Venus")}>Venus</li>
-    <li on:click={findPlanet("Earth")}>Earth</li>
-    <li on:click={findPlanet("Mars")}>Mars</li>
-    <li on:click={findPlanet("Jupiter")}>Jupiter</li>
-    <li on:click={findPlanet("Saturn")}>Saturn</li>
-    <li on:click={findPlanet("Uranus")}>Uranus</li>
-    <li on:click={findPlanet("Neptune")}>Neptune</li>
-</ul>
+<div class="container">
+    <h1>The Planets</h1>
+    <ul>
+        <li on:click={findPlanet("Mercury")}>MERCURY</li>
+        <li on:click={findPlanet("Venus")}>VENUS</li>
+        <li on:click={findPlanet("Earth")}>EARTH</li>
+        <li on:click={findPlanet("Mars")}>MARS</li>
+        <li on:click={findPlanet("Jupiter")}>JUPITER</li>
+        <li on:click={findPlanet("Saturn")}>SATURN</li>
+        <li on:click={findPlanet("Uranus")}>URANUS</li>
+        <li on:click={findPlanet("Neptune")}>NEPTUNE</li>
+    </ul>
+</div>
+
+<style>
+    h1 {
+        display: inline
+    }
+    ul {
+        list-style: none;
+        display: flex;
+        width: max-content;
+    }
+    li {
+        cursor: pointer;
+
+    }
+    .container {
+        display: flex;
+        align-items: center;
+    }
+</style>
