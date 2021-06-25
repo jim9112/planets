e219912c402e860cec282d6418ce694455fb554c
jim9112 <jim9112@gmail.com>
2021-06-25 15:03:47 -0400

    separate overview component

---
+++ src/components/Overview.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    export let image;
+    export let name;
+    export let content;
+</script>
+
+<div class="planet-container">
+    <div class="image-container">
+        <img src={image} alt="">
+    </div>
+    <h1>{name}</h1>
+    <p>{content}</p>
+</div>
+<style>
+    .image-container {
+        margin-top: 20px;
+        width: 100%;
+        display: grid;
+        justify-content: center;
+    }
+    .planet-container {
+        text-align: center;
+        padding: 24px;
+    }
+    @media (max-width: 812px) {
+        img {
+            width: 200px;
+            margin: auto;
+        }
+    }
+</style>
--- src/components/PlanetCard.svelte
+++ src/components/PlanetCard.svelte
@@ -1,29 +1,7 @@
 <script lang="ts">
+    import Overview from '../components/Overview.svelte' 
     export let planet
 </script>
-<div class="planet-container">
-    <div class="image-container">
-        <img src={planet.images.planet} alt="">
-    </div>
-    <h1>{planet.name}</h1>
-    <p>{planet.overview.content}</p>
-</div>
 
-<style>
-    .image-container {
-        margin-top: 20px;
-        width: 100%;
-        display: grid;
-        justify-content: center;
-    }
-    .planet-container {
-        text-align: center;
-        padding: 24px;
-    }
-    @media (max-width: 812px) {
-        img {
-            width: 200px;
-            margin: auto;
-        }
-    }
-</style>
+
+<Overview image={planet.images.planet} name={planet.name} content={planet.overview.content} />
