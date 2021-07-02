46687b7aa24c120fba4e23b386c3cabfd214b220
jim9112 <jim9112@gmail.com>
2021-07-02 12:42:19 -0400

    moved planet to own component

--- src/components/Overview.svelte
+++ src/components/Overview.svelte
@@ -1,23 +1,17 @@
 <script lang="ts">
+    import PlanetImage from './PlanetImage.svelte';
     export let image: string;
     export let name: string;
     export let content: string;
     export let source: string;
     export let changeMode: (mode: string)=>{};
-    export let mode: 'overview' | 'internal' | 'geology'
+    export let mode: 'overview' | 'internal' | 'geology';
     export let secondImage: string | null;
-
 </script>
 
 <div class="planet-container">
     <div class="image-container">
-        <div class="relative">
-            <!-- to do: make planet its own component -->
-            <img class={`${name}-image`} src={image} alt="">
-            {#if secondImage !== null}
-                <img class="second-image-container" src={secondImage} alt="">
-            {/if}
-        </div>
+        <PlanetImage name={name} image={image} secondImage={secondImage} />
     </div>
     <div class="description-container">
         <div>
@@ -72,12 +66,7 @@
         display: grid;
         justify-content: center;
     }
-    .second-image-container {
-        position: absolute;
-        width: 168px;
-        left: 25%;
-        top: 50%;
-    }
+
     .planet-container {
         text-align: center;
         padding: 24px;
@@ -151,10 +140,6 @@
 
     /* phone */
     @media (max-width: 500px) {
-        img {
-            width: 200px;
-            margin: auto;
-        }
         .planet-container {
             grid-auto-flow: row;
             gap: 0px;
@@ -185,33 +170,6 @@
             gap: 60px;
             align-items: center;
         }
-
-        /* planet sizing */
-
-        .Mercury-image {
-            width: 180px;
-        }
-        .Venus-image {
-            width: 253px;
-        }
-        .Earth-image {
-            width: 285px;
-        }
-        .Mars-image {
-            width: 213px;
-        }
-        .Jupiter-image {
-            width: 369px;
-        }
-        .Saturn-image {
-            width: 422px;
-        }
-        .Uranus-image {
-            width: 290px;
-        }
-        .Neptune-image {
-            width: 285px;
-        }
     }
 
     /* computer */
@@ -234,33 +192,6 @@
             grid-template-columns: repeat(1, minmax(0, 1fr));
             max-width: 350px;
             gap: 39px;
-        }
-
-        /* planet sizing */
-
-        .Mercury-image {
-            width: 100%;
-        }
-        .Venus-image {
-            width: 100%;
-        }
-        .Earth-image {
-            width: 100%;
-        }
-        .Mars-image {
-            width: 100%;
-        }
-        .Jupiter-image {
-            width: 100%;
-        }
-        .Saturn-image {
-            width: 100%;
-        }
-        .Uranus-image {
-            width: 100%;
-        }
-        .Neptune-image {
-            width: 100%;
         }
     }
 </style>
---
+++ src/components/PlanetImage.svelte
@@ -0,0 +1,98 @@
+<script lang="ts">
+  export let name: string;
+  export let image: string;
+  export let secondImage: null | string;
+</script>
+
+
+<div class="relative">
+  <!-- to do: make planet its own component -->
+  <img class={`${name}-image`} src={image} alt="">
+  {#if secondImage !== null}
+      <img class="second-image-container" src={secondImage} alt="">
+  {/if}
+</div>
+
+<style>
+  .relative {
+        position: relative;
+    }
+
+    .second-image-container {
+        position: absolute;
+        width: 168px;
+        left: 25%;
+        top: 50%;
+    }
+    /* responsive breakpoints */
+
+    /* phone */
+    @media (max-width: 500px) {
+        img {
+            width: 200px;
+            margin: auto;
+        }
+    }
+    
+
+    /* tablet */
+    @media (min-width: 651px) {
+
+        /* planet sizing */
+
+        .Mercury-image {
+            width: 180px;
+        }
+        .Venus-image {
+            width: 253px;
+        }
+        .Earth-image {
+            width: 285px;
+        }
+        .Mars-image {
+            width: 213px;
+        }
+        .Jupiter-image {
+            width: 369px;
+        }
+        .Saturn-image {
+            width: 422px;
+        }
+        .Uranus-image {
+            width: 290px;
+        }
+        .Neptune-image {
+            width: 285px;
+        }
+    }
+
+    /* computer */
+    @media (min-width: 801px) {
+        /* planet sizing */
+
+        .Mercury-image {
+            width: 100%;
+        }
+        .Venus-image {
+            width: 100%;
+        }
+        .Earth-image {
+            width: 100%;
+        }
+        .Mars-image {
+            width: 100%;
+        }
+        .Jupiter-image {
+            width: 100%;
+        }
+        .Saturn-image {
+            width: 100%;
+        }
+        .Uranus-image {
+            width: 100%;
+        }
+        .Neptune-image {
+            width: 100%;
+        }
+    }
+</style>
